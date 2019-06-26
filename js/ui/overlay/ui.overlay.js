abc62f9d8550b2e8b78a7648f7e9e36f44e33663
tilliwilli <tilliwilli@gmail.com>
2019-06-26 15:12:46 +0300

    к контекстному меню dataGrid'a теперь применяется скейл

--- js/ui/overlay/ui.overlay.js
+++ js/ui/overlay/ui.overlay.js
@@ -1322,6 +1322,16 @@ var Overlay = Widget.inherit({
             var position = this._transformStringPosition(this._position, POSITION_ALIASES),
                 resultPosition = positionUtils.setup(this._$content, position);
 
+            // eсли это контекстное меню dataGrid'a то применяем коррекцию скейла
+            // без этой коррекции: размер меню будет оставаться слишком огромным при scale << 1
+            if(this._$content.hasClass("dx-context-menu") && this._$content.hasClass("dx-datagrid")) {
+                this._$content.css({
+                    "transform-origin": "top left",
+                    // преобразуем "translate(129px, 325px)" -> "translate(129px, 325px) scale(0.82)"
+                    "transform": `${this._$content[0].style.transform} scale(${devTools.scaleCorrection.scale()})`,
+                });
+            }
+
             forceRepaint(this._$content);
 
             // TODO: hotfix for T338096
